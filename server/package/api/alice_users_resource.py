b85989bdcfca4d3ee7c3dd81f8c99871f3a439ad
Zyryanova Maria <mariazyranova72@gmail.com>
2020-05-11 03:35:00 +0500

    Исправление ошибок

--- server/package/api/alice_users_resource.py
+++ server/package/api/alice_users_resource.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from flask_restful import reqparse, abort, Resource
-from server.data import db_session
-from server.data.alice_users import AliceUser
+from package.data import db_session
+from package.data.alice_users import AliceUser
 
 parser = reqparse.RequestParser()
 parser.add_argument('name', required=True, type=str)
--- server/package/api/category_resource.py
+++ server/package/api/category_resource.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from flask_restful import reqparse, abort, Resource
-from server.data import db_session
-from server.data.categories import Category
+from package.data import db_session
+from package.data.categories import Category
 
 parser = reqparse.RequestParser()
 parser.add_argument('name', required=True, type=str)
